5b6c1a5dd952720102e38a1e80e1f72d317600b8
ural <mametovural@gmail.com>
2024-06-18 18:07:25 +0500

    made it a bit more responsive with the slider

--- javascript/script.js
+++ javascript/script.js
@@ -4,7 +4,7 @@ let accMenu = document.querySelectorAll(".accordion__menu-title"),
 accMenu.forEach((item, i) => {
   item.addEventListener("click", () => {
     accMenu[i].classList.toggle("active");
-    
+
     let content = contentMenu[i];
 
     if (content.style.maxHeight) {
@@ -17,4 +17,38 @@ accMenu.forEach((item, i) => {
         ((c.style.maxHeight = null), accMenu[j].classList.remove("active"));
     });
   });
+});
+
+const img = document.querySelector(".slider-container");
+const imgs = document.querySelectorAll(".slider-item");
+const nextBtn = document.querySelector(".slider-btn__next");
+const prevBtn = document.querySelector(".slider-btn__prev");
+
+let idx = 0;
+
+function changeImg() {
+  if (idx > imgs.length - 1) {
+    idx = 0;
+  } else if (idx < 0) {
+    idx = imgs.length - 1;
+  }
+  img.style.transform = `translateX(${-idx * 70}px)`;
+}
+nextBtn.addEventListener("click", () => {
+  idx++;
+  changeImg();
+});
+prevBtn.addEventListener("click", () => {
+  idx--;
+  changeImg();
+});
+
+document.addEventListener("keydown", (e) => {
+  if (e.key == "d") {
+    idx++;
+    changeImg();
+  } else if (e.key == "a") {
+    idx--;
+    changeImg();
+  }
 });
